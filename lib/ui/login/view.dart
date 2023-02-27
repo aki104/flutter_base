e5a1f1958393fcd727de2f1104f9752f75c61ae1
山下明俊 <aki1041990@gmail.com>
2023-02-28 01:22:50 +0900

    作り直し

---
+++ lib/ui/login/view.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../../widget/validation_form/validation_form.dart';
+import 'notifier.dart';
+
+class LoginPage extends ConsumerWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final state = ref.watch(loginProvider);
+    final notifier = ref.read(loginProvider.notifier);
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ValidationInputForm(
+                controller: state.mailController,
+                errorMsg: state.mailErrorMsg,
+                hintText: 'メールアドレス',
+                inputType: TextInputType.emailAddress,
+              ),
+              ValidationInputForm(
+                controller: state.passwordController,
+                errorMsg: state.passwordErrorMsg,
+                hintText: 'パスワード',
+                inputType: TextInputType.emailAddress,
+              ),
+              ValidationInputForm(
+                controller: state.confPasswordController,
+                errorMsg: state.confPasswordErrorMsg,
+                hintText: 'パスワード再入力',
+                inputType: TextInputType.emailAddress,
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  notifier.onTapLoginButton();
+                },
+                child: const Text('ログイン'),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              // Text(
+              //   state.isValidation ? 'バリデーションエラー' : 'OK',
+              //   style: const TextStyle(color: Colors.pink),
+              // )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
---
+++ lib/util/validation/validation.dart
@@ -0,0 +1,56 @@
+import 'package:base_app/util/validation/validation_type.dart';
+
+class Validation {
+  const Validation._();
+
+
+  String validation(
+      {required List<Validator> validationList,
+        required String value,
+        String? value2}) {
+    List<String> message = [];
+    for (var validator in validationList) {
+      if (!validator.validate(value, value2)) {
+        message.add(validator.getMessage());
+      }
+    }
+
+    return message.isEmpty ? '' : message.first;
+  }
+
+  //全てのバリデーション判定
+  bool isAllValid({required List<String> messageList}) {
+    bool isValid = false;
+    for (var massage in messageList) {
+      if (massage.isNotEmpty) {
+        isValid = true;
+      }
+    }
+
+    return isValid;
+  }
+
+
+}
+
+enum InputFormType {
+  mail,
+  password,
+  rePassword,
+}
+
+extension InputFormTypeEx on InputFormType {
+  List<Validator> get validatorList {
+    switch (this) {
+      case InputFormType.mail:
+        return [RequiredValidator(), EmailValidator()];
+      case InputFormType.password:
+        return [RequiredValidator(), ContainsAlphanumericalValidator()];
+      case InputFormType.rePassword:
+        return [RequiredValidator(), MachStringValidator()];
+    }
+  }
+}
+
+
+
